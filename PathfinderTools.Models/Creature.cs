41f98a437f5f3eb222b5c510e318232708e15837
Mark Dewey <mark.t.dewey@gmail.com>
2017-09-26 17:10:44 -0400

    added monster class, needs navigation and seed data

---
+++ PathfinderTools.Models/Creature.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PathfinderTools.Models
+{
+    public class Creature
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int HitPoints { get; set; }
+        public int HitDie { get; set; }
+        public int Types { get; set; }
+        public string Size { get; set; }
+        public string Tatics { get; set; }
+        public string Morale { get; set; }
+        public string SpecialAbilities { get; set; }
+        public int ChallengeRating { get; set; }
+        public string Alignment { get; set; }
+        public string PiazoUrl { get; set; }
+        public int Init { get; set; }
+        public string Senses { get; set; }
+        public string Skills { get; set; }
+        public int Speed { get; set; }
+        public int Ac { get; set; }
+        public int FlatFooted { get; set; }
+        public int Touch { get; set; }
+        public string Weakness { get; set; }
+        public string Immunities { get; set; }
+        public string DamageReduction { get; set; }
+        public string Attacks { get; set; }
+        public int Fort { get; set; }
+        public int Reflex { get; set; }
+        public int Will { get; set; }
+        public int Strength { get; set; }
+        public int Dexerity { get; set; }
+        public int Constitution { get; set; }
+        public int Wisdom { get; set; }
+        public int Intelligence { get; set; }
+        public int Charisma { get; set; }
+        public string Feats { get; set; }
+        public string Languages { get; set; }
+        public string Attitude { get; set; }
+        public int CMB { get; set; }
+        public int CMD { get; set; }
+        public int XP { get; set; }
+        public string Treasure { get; set; }
+
+    }
+}
