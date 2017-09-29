712f0414772a012bdaf60ba4d6b579a34df5b901
Mark Dewey <mark.t.dewey@gmail.com>
2017-09-29 12:18:54 -0400

    added new compnemt for creation

---
+++ PathfinderTools/js/components/createDungeon.jsx
@@ -0,0 +1,34 @@
+﻿import React from 'react';
+import { Link } from 'react-router-dom';
+
+//THought I shouldnt have any ajax calls in here
+class CreateDungeon extends React.Component {
+
+    constructor(props) {
+        super(props);
+        console.log(["room", "ctor", props]);
+        this.state = {
+
+        };
+    };
+
+    componentWillReceiveProps(nextProps) {
+        console.log(['room', 'props', nextProps, this.props, this.state])
+        this.setState((p, n) => {
+            return {
+                currentRoom: nextProps.selectedRoom,
+                dungeon: nextProps.dungeon
+            }
+        })
+    }
+
+    render() {
+        console.log(['create', 'render', this.state])
+        return <section>
+            lets create something
+           </section>
+    } // end of render
+} //end of class
+
+
+module.exports = CreateDungeon;
--- PathfinderTools/js/index.jsx
+++ PathfinderTools/js/index.jsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import Dungeons from './components/dungeons'
 import SelectedDungeon from './components/selectedDungeon'
+import CreateDungeon from './components/createDungeon'
 import { Switch, Route } from 'react-router-dom'
 
 class App extends React.Component {
@@ -18,6 +19,7 @@ class App extends React.Component {
             <Switch>
                 <Route exact path='/' component={Dungeons} />
                 <Route exact path='/dungeon/:id/room/:roomId' component={SelectedDungeon} />
+                <Route exact path='/dungeon/create' component={CreateDungeon}/>
             </Switch>
         </main>
     }
