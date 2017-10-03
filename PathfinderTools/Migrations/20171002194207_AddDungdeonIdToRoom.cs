3037fd0353585144342254dcc09f26d326b04645
Mark Dewey <mark.t.dewey@gmail.com>
2017-10-03 09:53:31 -0400

    added files

---
+++ PathfinderTools/Migrations/20171002194207_AddDungdeonIdToRoom.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PathfinderTools.Migrations
+{
+    public partial class AddDungdeonIdToRoom : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DungeonId",
+                table: "Rooms",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_DungeonId",
+                table: "Rooms",
+                column: "DungeonId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rooms_Dungeons_DungeonId",
+                table: "Rooms",
+                column: "DungeonId",
+                principalTable: "Dungeons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rooms_Dungeons_DungeonId",
+                table: "Rooms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rooms_DungeonId",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "DungeonId",
+                table: "Rooms");
+        }
+    }
+}
